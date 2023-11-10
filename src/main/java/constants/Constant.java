b196880de6c0117321bc500197cc34f4badc1d87
Iuliia Rudi <connection454@gmail.com>
2023-11-09 22:11:11 -0500

    pom settings

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -1,11 +1,18 @@
 package constants;
 
 
-public class Contant {
+public class Constant {
 
     public static final String BASE_URL = "https://restful-booker.herokuapp.com/booking";
 
+    /* File constants*/
     public static final String BASE_FILE_PATH = "src\\test\\resources\\";
     public static final String BASE_API_REQUIEST_BODY = BASE_FILE_PATH + "createBookingPostRequest.txt";
     public static final String BOOKING_JSON_SCHEMA = BASE_FILE_PATH + "bookingJsonValidationSchema.txt";
+
+    /* Json path constants */
+
+    public static final String FIRST_NAME_JPATH = "booking.firstname";
+    public static final String LAST_NAME_JPATH = "booking.lastname";
+    public static final String CHECKIN_DATE_JPATH = "booking.bookingdates.checkin";
 }
